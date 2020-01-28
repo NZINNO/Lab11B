a87268feb5d788e74ec0d95f58ba32c26a358449
Zinno <4070806@VTSD.COM>
2020-01-28 09:10:14 -0500

    you are going to have fun with this one!

--- Lab11bvst.java
+++ Lab11bvst.java
@@ -7,7 +7,10 @@ public class Lab11bvst
 	public static void main(String[] args)
 	{
       Deck deck = new Deck();
-      System.out.println(deck);
+      deck.add("Clubs","Three",3);
+			deck.display();
+			System.out.println(deck.size());
+			System.out.println(deck.isEmpty());
 	}
 }
 
@@ -16,12 +19,23 @@ class Deck
 {
 	private Card[] cards;
 	private int size;
-	private String[] suits = {"Clubs","Diamonds","Hearts","Spades"};
-	private String[] rank = {"Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King","Ace"};
-	private String[] value = {"2","3","4","5","6","7","8","9","10","11"};
+	private String suits[] = {"Clubs","Diamonds","Hearts","Spades"};
+	private String rank[] = {"Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King","Ace"};
+	private int value[] = {2,3,4,5,6,7,8,9,10,10,10,10,11};
 	public Deck()
+}
+	public void add(String suit, String rank, int value)
    {
-      size = 52;
-		cards = new Card[size];
-	}
-	 }
+		 Card temp = new Card(suit, rank,value);
+		 cards[size] = temp
+		 size++;
+		}
+		public void display()
+		{
+			for(int k = 0; k<size; k++)
+				System.out.println(cards[k]);
+			System.out.println();
+		}
+
+	 		public int size()		{ return size; }
+			public boolean isEmpty() { return size == 0; }
